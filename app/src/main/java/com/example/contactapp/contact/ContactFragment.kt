6c67bc53fb7e1b1c1955a2f4891c730e0ded4792
choi <dacafo77@nate.com>
2024-07-22 22:20:40 +0900

    add : ContactFragment 추가

--- app/src/main/java/com/example/contactapp/contact/ContactFragment.kt
+++ app/src/main/java/com/example/contactapp/contact/ContactFragment.kt
@@ -1,12 +1,30 @@
 package com.example.contactapp.contact
 
 import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.contactapp.R
+import com.example.contactapp.databinding.FragmentContactBinding
 
-class ContactFragment:Fragment(R.layout.fragment_contact) {
-    override fun onViewStateRestored(savedInstanceState: Bundle?) {
-        super.onViewStateRestored(savedInstanceState)
+class ContactFragment : Fragment(R.layout.fragment_contact) {
+
+    private var binding: FragmentContactBinding? = null
+    private lateinit var articleAdapter: ArticleAdapter
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val fragmentContactBinding = FragmentContactBinding.bind(view)
+        binding = fragmentContactBinding
+
+        articleAdapter = ArticleAdapter()
+
+        fragmentContactBinding.articleRecyclerView.layoutManager = LinearLayoutManager(context)
+        fragmentContactBinding.articleRecyclerView.adapter = articleAdapter
     }
 
+
 }
+
+
+
